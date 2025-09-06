01dbae5fe2c00e7208e80fcc1c999828909d0567
imkhoat <khoatbui.idea@gmail.com>
2025-09-06 09:32:22 +0700

    feat(web): setup web project

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -2,9 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-red-100">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Welcome to Nativewind Miguel!
       </Text>
     </View>
   );
