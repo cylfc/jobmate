899bb852d27464b55894259011e5a35f92f97f46
imkhoat <khoatbui.idea@gmail.com>
2025-09-20 12:58:33 +0700

    feat(web): implement app layout

---
+++ web/app/components/layout-panel.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-full flex flex-1">
+    <UDashboardPanel id="layout-panel-left" class="flex-none" :resizable="true" :max-size="45" :min-size="0">
+      <template #header>
+        <slot name="header" />
+      </template>
+      <template #body>
+        <slot name="body" />
+      </template>
+      <template #footer>
+        <slot name="footer" />
+      </template>
+    </UDashboardPanel>
+    <div class="flex-1 bg-bg-200">
+      <slot />
+    </div>
+  </div>
+</template>
